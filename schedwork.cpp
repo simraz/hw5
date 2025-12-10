87bbec43f79806f77e577bd59ecf723310a63b22
Simra Zargar <szargar@usc.edu>
2025-12-10 05:53:00 +0000

    hw5 complete

--- schedwork.cpp
+++ schedwork.cpp
@@ -6,7 +6,6 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-// add or remove necessary headers as you please
 
 #endif
 
@@ -19,27 +18,69 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-
-// Add prototypes for any helper functions here
-
-
-// Add your implementation of schedule() and other helper functions here
-
-bool schedule(
-    const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
-    DailySchedule& sched
-)
+bool scheduleHelper(size_t day, size_t workerSlot, const AvailabilityMatrix& avail,
+                const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, std::vector<size_t>& shiftsWorked)
 {
-    if(avail.size() == 0U){
-        return false;
+    size_t nDays = avail.size();
+    size_t kWorkers = avail[0].size();
+
+    // Base case: All days scheduled
+    if (day == nDays) {
+        return true;
     }
-    sched.clear();
-    // Add your code below
 
+    // Determine next position to fill
+    size_t nextDay = (workerSlot + 1 == dailyNeed) ? day + 1 : day;
+    size_t nextWorkerSlot = (workerSlot + 1) % dailyNeed;
 
+    // Iterate through all possible workers for the current slot
+    for (Worker_T workerId = 0; workerId < kWorkers; workerId++) {
+        // Is worker available
+        if (!avail[day][workerId]) {
+            continue;
+        }
 
+        // Is worker already scheduled for today
+        auto endFilledIt = sched[day].begin() + workerSlot;
+        auto it = std::find(sched[day].begin(), endFilledIt, workerId);
+        if (it != endFilledIt) {
+            continue;
+        }            
 
+        // Has worker reached max shifts
+        if (shiftsWorked[workerId] >= maxShifts) {
+            continue;
+        }
+
+        // Assign worker to the current slot
+        sched[day][workerSlot] = workerId;
+        shiftsWorked[workerId]++;
+
+        // Do recursion to fill in the next slot
+        if (scheduleHelper(nextDay, nextWorkerSlot, avail, dailyNeed, maxShifts, sched, shiftsWorked)) {
+            // return if successfully scheduled
+            return true;
+        }
+
+        // Do back tracking if not successful
+        shiftsWorked[workerId]--;
+    }
+
+    // No worker could be assigned to this slot
+    return false;
+}
+
+bool schedule(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched)
+{
+    size_t nDays = avail.size();
+    size_t kWorkers = avail[0].size();
+
+    // Initialize schedule with empty values
+    sched = std::vector<std::vector<Worker_T>>(nDays, std::vector<Worker_T>(dailyNeed));
+
+    // Keep track of shifts worked by each worker
+    std::vector<size_t> shiftsWorked(kWorkers, 0);
+
+    return scheduleHelper(0, 0, avail, dailyNeed, maxShifts, sched, shiftsWorked);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+static void wordle_helper(size_t pos, std::string pattern, std::string remaining, const std::set<std::string>& dict, 
+        size_t numBlanks, std::set<std::string>& results);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +22,62 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    std::set<std::string> results;
+    int numBlanks = 0;
+    for (size_t i = 0; i < in.size(); i++) {
+        if (in[i] == '-') {
+            numBlanks++;
+        }
+    }
+
+    wordle_helper(0, in, floating, dict, numBlanks, results);
+    return results;
 
 }
 
 // Define any helper functions here
+static void wordle_helper(size_t pos, std::string pattern, std::string remaining,
+                  const std::set<std::string>& dict, size_t numBlanks, std::set<std::string>& results) {
+
+    // Base case: reached end of pattern
+    if (pos == pattern.size()) {
+        // All floating must be used
+        if (!remaining.empty()) {
+            return;
+        }
+        // Pattern completed - check if the word is in dict. Add to results.
+        if (dict.find(pattern) != dict.end()) {
+            results.insert(pattern);
+        }
+        return;
+    }
+
+    // Base case: Too many floating for remaining blanks
+    if (remaining.size() > numBlanks) {
+        return;
+    }
+
+    // Recursive: If current position is a fixed letter
+    if (pattern[pos] != '-') {
+        wordle_helper(pos + 1, pattern, remaining, dict, numBlanks, results);
+        return;
+    }
+
+
+    // Recursive case: try each remaining floating letter for this blank
+    for (size_t i = 0; i < remaining.size(); i++) {
+        pattern[pos] = remaining[i];
+        // remove this character from remaining
+        std::string nextRemaining = remaining.substr(0, i) + remaining.substr(i + 1);
+        wordle_helper(pos + 1, pattern, nextRemaining, dict, numBlanks - 1, results);
+    }
+
+    // Recursive case: try all letters a-z for this blank
+    std::string alphabets_str = "abcdefghijklmnopqrstuvwxyz";
+    for (size_t i = 0; i < alphabets_str.size(); i++) {
+        if (remaining.find(alphabets_str[i]) == string::npos) {
+            pattern[pos] = alphabets_str[i];
+            wordle_helper(pos + 1, pattern, remaining, dict, numBlanks - 1, results);
+        }
+    }
+}
